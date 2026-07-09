b9536b6e6fc9a6daf2e36bd9164145909568da48
ProjetosPotencial <projetos@potencialgrupo.com.br>
2026-07-09 10:13:53 -0300

    update18

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,6 +6,9 @@ import { obterPeriodoAtual } from "@/lib/date-utils";
 import Sidebar from "@/components/sidebar";
 import TopNav from "@/components/topnav";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
   const supabase = createClient();
   const { data: { session } } = await supabase.auth.getSession();
